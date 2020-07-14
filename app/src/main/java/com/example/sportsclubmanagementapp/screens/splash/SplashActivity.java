72ce8d3572819b0b0a14a3c56174de3df0d2505c
pro119 <dumitrumanea119@gmail.com>
2020-07-14 09:11:39 +0300

    splash screen

--- app/src/main/java/com/example/sportsclubmanagementapp/screens/splash/SplashActivity.java
+++ app/src/main/java/com/example/sportsclubmanagementapp/screens/splash/SplashActivity.java
@@ -2,15 +2,38 @@ package com.example.sportsclubmanagementapp.screens.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.sportsclubmanagementapp.R;
+import com.example.sportsclubmanagementapp.screens.guest.GuestActivity;
+
+import gr.net.maroulis.library.EasySplashScreen;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        EasySplashScreen config = new EasySplashScreen(SplashActivity.this)
+                .withFullScreen()
+                .withTargetActivity(GuestActivity.class)
+                .withSplashTimeOut(5000)
+                .withBackgroundColor(Color.parseColor("#1a1a1a"))
+                .withAfterLogoText("SPORTS CLUB")
+                .withLogo(R.mipmap.ic_launcher_round);
+
+        config.getAfterLogoTextView().setTextColor(Color.WHITE);
+        config.getAfterLogoTextView().setTextSize(22);
+        config.getAfterLogoTextView().setLetterSpacing((float)0.06);
+        View easySplashScreen = config.create();
+
+        setContentView(easySplashScreen);
     }
+
+
 }
