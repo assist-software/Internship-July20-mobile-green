b7fecdf4d4dcedc9844e5fb2a5d7634ce3bd691a
SILITRA Mihai-Andrei <silitramihai1999@gmail.com>
2020-07-14 12:43:49 +0300

    login activity implemented

--- app/src/main/java/com/example/sportsclubmanagementapp/screens/login/LoginActivity.java
+++ app/src/main/java/com/example/sportsclubmanagementapp/screens/login/LoginActivity.java
@@ -1,34 +1,25 @@
-package com.example.sportsclubmanagementapp.screens.guest;
+package com.example.sportsclubmanagementapp.screens.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.sportsclubmanagementapp.R;
-import com.example.sportsclubmanagementapp.screens.splash.SplashActivity;
 
-public class GuestActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guest);
-        Button login_btn = findViewById(R.id.login_btn);
+        setContentView(R.layout.activity_login);
+
         Button register_btn = findViewById(R.id.register_btn);
 
         login_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //startActivity(new Intent(GuestActivity.this, LoginActivity.class));
-            }
-        });
-
-        register_btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //startActivity(new Intent(GuestActivity.this, RegisterActivity.class));
             }
         });
     }
